fb5d2f1f8d15c5bc3df0bf83082e663a8eaf06c6
williamson922 <100593891+williamson922@users.noreply.github.com>
2023-08-14 17:03:22 +0800

    function save and load data

--- ML_API/api.py
+++ ML_API/api.py
@@ -24,7 +24,7 @@ cursor = connection.cursor()
 
 # Define the required features for the model
 required_features = ["Date/Time", "Voltage Ph-A Avg", "Voltage Ph-B Avg", "Voltage Ph-C Avg", "Current Ph-A Avg", "Current Ph-B Avg", "Current Ph-C Avg"
-                     ,"Power Factor Total","Power Ph-A","Power Ph-B","Power Ph-C", "Total Power","Unix Timestamp"]
+                     ,"Power Factor Total","Power Ph-A","Power Ph-B","Power Ph-C", "Total Power","Unix Timestamp","predicted power"]
 
 def load_lstm_model(version):
     # weekday model folder path
@@ -65,23 +65,114 @@ def load_svr_model(model_type, version):
     return model
 
 
-def save_data_database(data):
+def save_data_database(data, is_first=True, is_prediction=False):
     try:
-        # Extract the required columns from the preprocessed_data DataFrame
-        values = data[required_features].values.tolist()
+        if not is_prediction:
+            if is_first:
+                # Create date range
+                datetime_start = data['Date/Time'].iloc[0]  # Get the first datetime value
+                today = pd.date_range(datetime_start, periods=48, freq='30T')
 
-        # Define the INSERT query
-        query = "INSERT INTO Energy_Data (`Date/Time`, `Voltage Ph-A Avg`, `Voltage Ph-B Avg`, `Voltage Ph-C Avg`, `Current Ph-A Avg`, `Current Ph-B Avg`, `Current Ph-C Avg`, `Power Factor Total`, `Power Ph-A`, `Power Ph-B`, `Power Ph-C`, `Total Power`, `Unix Timestamp`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                # Convert the datetime values to the MySQL DATETIME format
+                formatted_dates = [dt.strftime('%Y-%m-%d %H:%M:%S') for dt in today]
 
-        # Execute the INSERT query with the list of values
-        cursor.executemany(query, values)
+                # Prepare the data for insertion (formatted datetime values)
+                insertion_data = [(formatted_date,) for formatted_date in formatted_dates]
 
-        connection.commit()
+                # Define the INSERT query for inserting interval datetimes
+                insert_query = "INSERT INTO testing (`Date/Time`) VALUES (%s)"
 
+                # Execute the INSERT query to insert interval datetimes
+                cursor.executemany(insert_query, insertion_data)
+                connection.commit()
+            update_query = """
+            UPDATE testing 
+            SET `Voltage Ph-A Avg`= %s,
+                `Voltage Ph-B Avg`= %s,
+                `Voltage Ph-C Avg`= %s,
+                `Current Ph-A Avg`= %s,
+                `Current Ph-B Avg`= %s,
+                `Current Ph-C Avg`= %s,
+                `Power Factor Total`= %s,
+                `Power Ph-A`= %s,
+                `Power Ph-B`= %s,
+                `Power Ph-C`= %s,
+                `Total Power`= %s,
+                `Unix Timestamp` = %s 
+            WHERE `Date/Time` = %s
+            """
+
+            # Assuming 'data' contains the features and datetime value
+            for _, row in data.iterrows():
+                update_data = (
+                    row['Voltage Ph-A Avg'], row['Voltage Ph-B Avg'], row['Voltage Ph-C Avg'],
+                    row['Current Ph-A Avg'], row['Current Ph-B Avg'], row['Current Ph-C Avg'],
+                    row['Power Factor Total'], row['Power Ph-A'], row['Power Ph-B'],
+                    row['Power Ph-C'], row['Total Power'],row['Unix Timestamp'], row['Date/Time']
+                )
+            
+            cursor.execute(update_query, update_data)
+            connection.commit()
+                
+            # Update the prediction data based on DateTime
+        if is_prediction:
+            # Update query for predicted values
+            update_query = f"UPDATE testing SET `predicted power` = %s WHERE `Date/Time` = %s"
+            
+            # Prepare data for updating
+            update_data = [(row['Predicted Load'], pd.to_datetime(row['Date/Time'])) for _, row in data.iterrows()]
+
+            # Execute the UPDATE query to update predicted power values
+            cursor.executemany(update_query, update_data)
+            connection.commit()
     except Exception as e:
         connection.rollback()
         raise ValueError("Error saving data to database: " + str(e))
+    
+def load_data(date):
+    try:
+        date_only = date[0].date()  # Get the date part using the .date() method
+        print("Date Only:", date_only)
 
+        query = """
+                SELECT
+                    `Date/Time`, `Voltage Ph-A Avg`, `Voltage Ph-B Avg`, `Voltage Ph-C Avg`,
+                    `Current Ph-A Avg`, `Current Ph-B Avg`, `Current Ph-C Avg`,
+                    `Power Factor Total`, `Power Ph-A`, `Power Ph-B`, `Power Ph-C`, `Total Power`, `predicted power`
+                FROM testing WHERE DATE(`Date/Time`) = %s ORDER BY `Date/Time` ASC
+                """
+        cursor.execute(query, (date_only,))
+        
+        data = cursor.fetchall()
+        print("Data:", data)
+        if data:
+            actual_dict = {}
+            predicted_dict = {}
+            for row in data:
+                actual_dict[str(row[0])] = {  # Convert datetime object to string
+                    "Voltage Ph-A Avg": row[1],
+                    "Voltage Ph-B Avg": row[2],
+                    "Voltage Ph-C Avg": row[3],
+                    "Current Ph-A Avg": row[4],
+                    "Current Ph-B Avg": row[5],
+                    "Current Ph-C Avg": row[6],
+                    "Power Factor Total": row[7],
+                    "Power Ph-A": row[8],
+                    "Power Ph-B": row[9],
+                    "Power Ph-C": row[10],
+                    "Total Power": row[11],
+                }
+                predicted_dict[str(row[0])] = {  # Convert datetime object to string
+                    "predicted": row[12],
+                }
+
+            return actual_dict, predicted_dict  # Return separate dictionaries for actual and predicted data
+
+        return {}, {}  # Return empty dictionaries if no data found for the specified date
+
+    except Exception as e:
+        print("Error loading data:", e)
+        return {}, {}
 
 def get_model_for_date(date, version):
     if date.weekday() < 5:
@@ -140,6 +231,9 @@ def add_features(data):
     return data
 
 def get_data_for_model(date):
+    column_features = ["Date/Time", "Voltage Ph-A Avg", "Voltage Ph-B Avg", "Voltage Ph-C Avg", "Current Ph-A Avg", "Current Ph-B Avg", "Current Ph-C Avg"
+                     ,"Power Factor Total","Power Ph-A","Power Ph-B","Power Ph-C", "Total Power","Unix Timestamp"]
+
     if date.weekday() >=5:
         query = "SELECT `Date/Time`, `Voltage Ph-A Avg`, `Voltage Ph-B Avg`, `Voltage Ph-C Avg`, `Current Ph-A Avg`, `Current Ph-B Avg`, `Current Ph-C Avg`, `Power Factor Total`, `Power Ph-A`, `Power Ph-B`, `Power Ph-C`, `Total Power`,`Unix Timestamp` FROM Energy_Data WHERE `Date/Time` BETWEEN (SELECT MAX(`Date/Time`) FROM Energy_Data WHERE WEEKDAY(`Date/Time`) = 5) AND (SELECT MAX(`Date/Time`) FROM Energy_Data WHERE WEEKDAY(`Date/Time`) = 6) ORDER BY `Date/Time` DESC LIMIT 48"
     elif date.weekday() < 5:
@@ -150,7 +244,7 @@ def get_data_for_model(date):
     if len(data) >= 48:
         # Since the query results are in descending order, reverse the data list to get the earliest date first
         data.reverse()
-        return pd.DataFrame(data, columns=required_features)
+        return pd.DataFrame(data, columns=column_features)
     else:
         return None
     
@@ -271,40 +365,58 @@ def predict():
             print("DF:", data)
             # Preprocess the data
             preprocessed_data = preprocess_data(data)
-            
-        save_data_database(preprocessed_data)    
-        # Extract the date from the timestamp and convert to datetime object
-        preprocessed_data["Date/Time"] = pd.to_datetime(preprocessed_data["Date/Time"])
-    
-        # Get the desired model version from the request parameters
-        version = request.args.get("version")
-
-        # Load the appropriate model based on the specified version
-        if version:
-            if version.startswith("lstm"):
-                model_to_use = load_lstm_model(version)
-            elif version.startswith("svr_weekend"):
-                model_to_use = load_svr_model("weekend", version)
-            elif version.startswith("svr_holiday"):
-                model_to_use = load_svr_model("holiday", version)
+            print('Preprocessed data:', preprocessed_data)
+            # Extract the date from the timestamp and convert to datetime object
+            preprocessed_data["Date/Time"] = pd.to_datetime(preprocessed_data["Date/Time"])
+            first_data = True
+            if (preprocessed_data['Date/Time'].dt.time != pd.Timestamp("00:00:00").time()).any():
+                first_data =False
+                save_data_database(preprocessed_data,is_first=False)
             else:
-                return jsonify({"error": "Invalid model version"}), 400
+                save_data_database(preprocessed_data)
+                
+        if first_data:        
+            # Extract the date from the timestamp and convert to datetime object
+            preprocessed_data["Date/Time"] = pd.to_datetime(preprocessed_data["Date/Time"])
+        
+            # Get the desired model version from the request parameters
+            version = request.args.get("version")
+
+            # Load the appropriate model based on the specified version
+            if version:
+                if version.startswith("lstm"):
+                    model_to_use = load_lstm_model(version)
+                elif version.startswith("svr_weekend"):
+                    model_to_use = load_svr_model("weekend", version)
+                elif version.startswith("svr_holiday"):
+                    model_to_use = load_svr_model("holiday", version)
+                else:
+                    return jsonify({"error": "Invalid model version"}), 400
+            else:
+                # If no version specified, use the default models
+                model_to_use = get_model_for_date(preprocessed_data["Date/Time"].iloc[0], version)
+        
+            
+            # Get the last weekend's data from the database
+            last_48_data = get_data_for_model(preprocessed_data["Date/Time"].iloc[0])
+            preprocessed_df = pd.DataFrame(last_48_data)
+            #Change the format of the date of actual preprocessed_data)
+            preprocessed_data['Date/Time'] = pd.to_datetime(preprocessed_data['Date/Time']).dt.strftime("%d-%m-%Y %H:%M")
+            # Use the predict_next_48_points method to make predictions
+            predictions_df = predict_next_48_points(model_to_use, preprocessed_df,preprocessed_data['Date/Time'])
+            print( predictions_df)
+            save_data_database(predictions_df,is_prediction=True)
+            print(preprocessed_data.to_dict(orient='records'))
+            # Return the predicted data and actual in the JSON response
+            return jsonify({"predicted_data": predictions_df.to_dict(orient="records"),
+                            'actual_data':preprocessed_data.to_dict(orient='records')}), 200
         else:
-            # If no version specified, use the default models
-            model_to_use = get_model_for_date(preprocessed_data["Date/Time"].iloc[0], version)
-    
-        
-        # Get the last weekend's data from the database
-        last_48_data = get_data_for_model(preprocessed_data["Date/Time"].iloc[0])
-        preprocessed_df = pd.DataFrame(last_48_data,columns=required_features)
-        #Change the format of the date of actual preprocessed_data)
-        preprocessed_data['Date/Time'] = pd.to_datetime(preprocessed_data['Date/Time']).dt.strftime("%d-%m-%Y %H:%M")
-        # Use the predict_next_48_points method to make predictions
-        predictions_df = predict_next_48_points(model_to_use, preprocessed_df,preprocessed_data['Date/Time'])
-        
-        # Return the predicted data in the JSON response
-        return jsonify({"predicted_data": predictions_df.to_dict(orient="records"),
-                        'actual_data':preprocessed_data.to_dict(orient='records')}), 200
+            actual_dict, predicted_dict = load_data(preprocessed_data['Date/Time'])
+            print("Actual data:", actual_dict)
+            print("Predicted data:", predicted_dict)
+            # Return the predicted data and actual in the JSON response
+            return jsonify({"predicted_data": predicted_dict,
+                            'actual_data':actual_dict}), 200
 
     except Exception as e:
         # Log the full traceback
